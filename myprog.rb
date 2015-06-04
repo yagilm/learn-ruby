65e3d100754c7795fc4600254a9ec0cc0ad7879c
yagilm <manos@tardix.info>
2015-06-04 15:32:14 +0200

    first commit

---
+++ myprog.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+#string1 = String.new("Hello World!")
+#puts string1
+
+# Method that returns string with a name
+def sayHello(name)
+#  result = "Hello " + name + "!"
+  # Using the ", the sequence #{expression} is replaced by the value of expression.
+#  result = "Hello #{name}!"   
+#  return result
+#since the value returned by a ruby method is the value of the last expression evaluated, we can get rid of the return statement altogether.
+  "Hello #{name}!"
+end
+
+puts sayHello("Manos")
